a49a86dd556b2c26b98ac64d3b73a940be849e62
L4CO <lapsansky.ladislav@gmail.com>
2023-09-29 23:54:10 +0200

    finished CDN

--- 03_step/cdn/componenet.js
+++ 03_step/cdn/componenet.js
@@ -3,12 +3,10 @@ import { ref, reactive } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 export default {
 
   setup() {
-    const counter = reactive({ count: 0 })
-    const message = ref('Hello World!')
+    const titleClass = ref('title')
 
     return {
-      counter,
-      message
+      titleClass
     }
   }
 }
