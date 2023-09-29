012e6bfec1238323f5b9739e201f1329b3ed9888
L4CO <lapsansky.ladislav@gmail.com>
2023-09-29 23:26:07 +0200

    sfc finished

--- 02_step/sfc/src/App.vue
+++ 02_step/sfc/src/App.vue
@@ -1,4 +1,11 @@
-<template>
-   <h1>Hello World!</h1>
-</template>
+<script setup>
+  import { reactive, ref } from 'vue'
 
+  const counter = reactive({ count: 0 })
+  const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
