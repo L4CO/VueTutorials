cde164ff6aba13566876802dd06f8ae4463a3059
L4CO <lapsansky.ladislav@gmail.com>
2023-10-01 12:23:11 +0200

    SFC finished

--- 07_step/sfc/src/App.vue
+++ 07_step/sfc/src/App.vue
@@ -2,16 +2,35 @@
   console.log('vue via vite');
   import { ref } from "vue";
 
-  const awesome = ref( false );
+  let id = 0;
+  const newTodo = ref('');
 
-  function toggle(){
-    awesome.value = !awesome.value;
+  const todos = ref([
+    { id: id++, text: 'Learn HTML' },
+    { id: id++, text: 'Learn JavaScript' },
+    { id: id++, text: 'Learn Vue' }
+  ]);
+
+  function addTodo(){
+      todos.value.push({ id: id++, text: newTodo.value });
+      newTodo.value = '';
+    }
+
+  function removeTodo(foundTodo){
+	todos.value = todos.value.filter((item) => item != foundTodo);
     }
 
 </script>
 
 <template>
-  <button @click="toggle">toggle</button>
-  <h1 v-if="awesome">Vue is awesome!</h1>
-  <h1 v-else>Oh no 😢</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>    
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
